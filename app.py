a1b3bc9a9cfaf8820eab0b151a3592324023e79e
JaimeVecina <145988059+JaimeVecina@users.noreply.github.com>
2023-11-27 17:21:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -278,7 +278,7 @@ with st.expander("Encuesta"):
         st.markdown("<h3 style='color: white; font-size: 2em;'>Muchas gracias y hasta la próxima 🖖</h3>", unsafe_allow_html=True)
 
         # Almacena las respuestas en un archivo CSV
-        with open(ruta_csv_respuestas, 'a') as file:
+        with open(ruta_csv_respuestas, 'a', encoding='utf-8', newline='') as file:
             file.write(f"{nombre_usuario},{respuesta_pregunta_1},{respuesta_pregunta_2},{respuesta_pregunta_3},{testimonio},{respuesta_pregunta_5}\n")
 
 #----------------------SIDEBAR------------------------------------------
